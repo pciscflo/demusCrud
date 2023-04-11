ce715a5165ef505e48d70e7c4e9952adcb1c4dd6
professor <pciscflo@upc.edu.pe>
2023-04-11 17:55:54 -0500

    11-04-v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+import { AuthorComponent } from './component/author/author.component';
+import { AuthorListarComponent } from './component/author/author-listar/author-listar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorComponent,
+    AuthorListarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/author/author-listar/author-listar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-listar',
+  templateUrl: './author-listar.component.html',
+  styleUrls: ['./author-listar.component.css']
+})
+export class AuthorListarComponent {
+
+}
